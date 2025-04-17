c1a5d7acf050f4636b9f64c17b903eb22bbb3e5c
Ethan Ligon <ligon@berkeley.edu>
2025-04-16 22:48:00 -0700

    Updates for 2025 project 4.

--- Project4/example_nutrition.py
+++ Project4/example_nutrition.py
@@ -1,48 +1,70 @@
-#!pip install -r requirements.txt
+ #%pip install -r requirements.txt
 
 import pandas as pd
 import cfe.regression as rgsn
 
-InputFiles = {'Expenditures':('1yVLriVpo7KGUXvR3hq_n53XpXlD5NmLaH1oOMZyV0gQ','Expenditures (2019-20)'),
-              'Prices':('1yVLriVpo7KGUXvR3hq_n53XpXlD5NmLaH1oOMZyV0gQ','Prices'),
-              'HH Characteristics':('1yVLriVpo7KGUXvR3hq_n53XpXlD5NmLaH1oOMZyV0gQ','HH Characteristics'),
-              'FCT':('1yVLriVpo7KGUXvR3hq_n53XpXlD5NmLaH1oOMZyV0gQ','FCT'),
-              'RDI':('1yVLriVpo7KGUXvR3hq_n53XpXlD5NmLaH1oOMZyV0gQ','RDI'),}
+Uganda_Data = '1yFWlP5N7Aowaj6t2roRSFFUC50aFD-RLBGfzGtqLl0w'
 
-from eep153_tools.sheets import read_sheets
-import numpy as np
 import pandas as pd
+import numpy as np
+from eep153_tools.sheets import read_sheets
 
-# Get prices
-p = read_sheets(InputFiles['Prices'][0],
-                sheet=InputFiles['Prices'][1])
+# Change 'Uganda_Data' to key of your own sheet in Sheets, above
+x = read_sheets(Uganda_Data,sheet='Food Expenditures (2019-20)')
+x = x.set_index(['i','t','m','j']).squeeze()
 
-p = p.set_index(['t','m'])
-p.columns.name = 'j'
 
-p = p.apply(lambda x: pd.to_numeric(x,errors='coerce'))
-p = p.replace(0,np.nan)
+# Now prices
+p = read_sheets(Uganda_Data,sheet='Food Prices (2019-20)').set_index(['t','m','j','u'])
 
-fct = read_sheets(InputFiles['FCT'][0],
-                  sheet=InputFiles['FCT'][1])
+# Compute medians of prices for particular time, place and unit
+p = p.groupby(['t','m','j','u']).median()
+
+# Just keep metric units
+p = p.xs('Kg',level="u").squeeze().unstack('j')
+
+# Get intersection of goods we have prices *and* expenditures for:
+jidx = p.columns.intersection(x.index.levels[-1])
+
+# Drop prices for goods we don't have expenditures for
+p = p[jidx].T
+
+# Household characteristics
+d = read_sheets(Uganda_Data,sheet="Household Characteristics")
+d.columns.name = 'k'
+
+# Fill blanks with zeros
+d = d.replace(np.nan,0)
+
+# Expenditures x may have duplicate columns
+x = x.T.groupby(['i','t','m','j']).sum()
+x = x.replace(0,np.nan) # Replace zeros with missing
+
+# Take logs of expenditures; call this y
+y = np.log(x)
+
+d.set_index(['i','t','m'],inplace=True)
+
+fct = read_sheets(Uganda_Data,sheet='FCT')
 
 fct = fct.set_index('j')
 fct.columns.name = 'n'
 
 fct = fct.apply(lambda x: pd.to_numeric(x,errors='coerce'))
 
-################## RDI, if available (consider using US) #####################
-rdi = read_sheets(InputFiles['RDI'][0],
-                  sheet=InputFiles['RDI'][1])
+rdi = read_sheets(Uganda_Data,sheet='RDI')
+
 rdi = rdi.set_index('n')
 rdi.columns.name = 'k'
 
-r = rgsn.read_pickle('../Project3/uganda_estimates.rgsn')  # Assumes you've already set this up e.g., in Project 3
+rdi = rdi.apply(lambda x: pd.to_numeric(x,errors='coerce'))
+
+# Assumes you've already set this up e.g., in Project 3
+r = rgsn.read_pickle('../Project3/uganda_estimates.rgsn')
 
 # Reference prices chosen from a particular time; average across place.
 # These are prices per kilogram:
-pbar = p.xs('2019-20',level='t').mean()
-pbar = pbar[r.beta.index] # Only use prices for goods we can estimate
+pbar = p.loc[r.beta.index].mean(axis=1).fillna(1) # Only use prices for goods we can estimate
 
 import numpy as np
 
@@ -61,20 +83,52 @@ qhat = qhat.loc[:,qhat.count()>0]
 
 qhat
 
-def my_prices(p0,p=pbar,j='Millet'):
+def my_prices(j,p0,p=pbar):
     """
-    Change price of jth good to p0, holding other prices fixed.
+    Change price of jth good to p0, holding other prices fixed at p.
     """
     p = p.copy()
     p.loc[j] = p0
     return p
 
+import matplotlib.pyplot as plt
 
-use = 'Millet'  # Good we want demand curve for
+use = 'Matoke'  # Good we want demand curve for
 
 # Vary prices from 50% to 200% of reference.
 scale = np.linspace(.5,2,20)
 
+# Demand for Matoke for household at median budget
+plt.plot([r.demands(xref,my_prices(use,pbar[use]*s,pbar))[use] for s in scale],scale)
+
+# Demand for Matoke for household at 25% percentile
+plt.plot([r.demands(xbar.quantile(0.25),my_prices(use,pbar[use]*s,pbar))[use] for s in scale],scale)
+
+# Demand for Matoke for household at 75% percentile
+plt.plot([r.demands(xbar.quantile(0.75),my_prices(use,pbar[use]*s,pbar))[use] for s in scale],scale)
+
+plt.ylabel(f"Price (relative to base of {pbar[use]:.2f})")
+plt.xlabel(f"Quantities of {use} Demanded")
+
+fig,ax = plt.subplots()
+
+scale = np.geomspace(.01,10,50)
+
+ax.plot(np.log(scale*xref),[r.expenditures(s*xref,pbar)/(s*xref) for s in scale])
+ax.set_xlabel(f'log budget (relative to base of {xref:.0f})')
+ax.set_ylabel(f'Expenditure share')
+ax.set_title('Engel Curves')
+
+fct
+
+# Create a new FCT and vector of consumption that only share rows in common:
+fct0,c0 = fct.align(qhat.T,axis=0,join='inner')
+print(fct0.index)
+
+# The @ operator means matrix multiply
+N = fct0.T@c0
+
+N  #NB: Uganda quantities are for previous 7 days
 
 def nutrient_demand(x,p):
     c = r.demands(x,p)
@@ -85,6 +139,29 @@ def nutrient_demand(x,p):
     
     return N
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.linspace(xref/5,xref*5,50)
+
+UseNutrients = ['Protein','Energy','Iron','Calcium','Vitamin C']
+
+df = pd.concat({myx:np.log(nutrient_demand(myx,pbar))[UseNutrients] for myx in X},axis=1).T
+ax = df.plot()
+
+ax.set_xlabel('log budget')
+ax.set_ylabel('log nutrient')
+
+USE_GOOD = 'Oranges'
+
+scale = np.geomspace(.01,10,50)
+
+ndf = pd.DataFrame({s:np.log(nutrient_demand(xref/2,my_prices(USE_GOOD,pbar[USE_GOOD]*s)))[UseNutrients] for s in scale}).T
+
+ax = ndf.plot()
+
+ax.set_xlabel('log price')
+ax.set_ylabel('log nutrient')
 
 # In first round, averaged over households and villages
 dbar = r.d[rdi.columns].mean()
@@ -94,3 +171,29 @@ dbar = r.d[rdi.columns].mean()
 hh_rdi = rdi@dbar
 
 hh_rdi
+
+def nutrient_adequacy_ratio(x,p,d,rdi=rdi,days=7):
+    hh_rdi = rdi.replace('',0)@d*days
+
+    return nutrient_demand(x,p)/hh_rdi
+
+X = np.geomspace(.01*xref,2*xref,100)
+
+pd.DataFrame({x:np.log(nutrient_adequacy_ratio(x,pbar,dbar))[UseNutrients] for x in X}).T.plot()
+plt.legend(UseNutrients)
+plt.xlabel('budget')
+plt.ylabel('log nutrient adequacy ratio')
+plt.axhline(0)
+plt.axvline(xref)
+
+scale = np.geomspace(.01,2,50)
+
+ndf = pd.DataFrame({s*pbar[USE_GOOD]:np.log(nutrient_adequacy_ratio(xref/4,my_prices(USE_GOOD,pbar[USE_GOOD]*s),dbar))[UseNutrients] for s in scale}).T
+
+fig,ax = plt.subplots()
+ax.plot(ndf['Vitamin C'],ndf.index)
+ax.axhline(pbar[USE_GOOD])
+ax.axvline(0)
+
+ax.set_ylabel('Price')
+ax.set_xlabel('log nutrient adequacy ratio')
